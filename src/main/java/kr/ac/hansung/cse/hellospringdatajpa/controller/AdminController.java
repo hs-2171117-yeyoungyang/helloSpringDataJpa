26f2bf4ece0e4e4a95089bae3ebc0f5fa8c571c5
yeyoung <yangyeyoung13@gmail.com>
2025-06-07 01:19:52 +0900

    feat: add logout and admin-only page

--- src/main/java/kr/ac/hansung/cse/hellospringdatajpa/controller/AdminController.java
+++ src/main/java/kr/ac/hansung/cse/hellospringdatajpa/controller/AdminController.java
@@ -1,10 +1,14 @@
 package kr.ac.hansung.cse.hellospringdatajpa.controller;
 
+import kr.ac.hansung.cse.hellospringdatajpa.entity.MyUser;
 import kr.ac.hansung.cse.hellospringdatajpa.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 public class AdminController {
@@ -12,9 +16,11 @@ public class AdminController {
     @Autowired
     private UserRepository userRepository;
 
-    @GetMapping("/admin")
-    public String adminHome(Model model) {
-        model.addAttribute("users", userRepository.findAll());
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @GetMapping("/admin/users")
+    public String listUsers(Model model) {
+        List<MyUser> users = userRepository.findAll();
+        model.addAttribute("users", users);
         return "adminhome";
     }
 }
